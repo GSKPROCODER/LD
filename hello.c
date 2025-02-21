8c8713bf59c18f73e4d02c37215df95781cd54e0
Gurjot Singh <gurjotgagan@gmail.com>
2025-02-21 12:28:58 +0530

    commit

--- hello.c
+++ hello.c
@@ -1,1 +1,22 @@
-hello
+#include <stdio.h>
+
+int main() {
+    int number, k = 2, i = 2;
+    
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    
+    // Calculate factorial starting from 2
+    // k starts at 2 and gets multiplied by each number from 2 up to number
+    while (i <= number) {
+        k = k * i;    // Multiply k by current number i
+        i = i + 1;    // Increment i to move to next number
+    }
+    
+    // Print the final result
+    printf("Result: %d\n", k);
+    
+    return 0;
+}
+
+
